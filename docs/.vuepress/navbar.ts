33a95b1eee1c153511868105eb72bfe60f69eb76
ofepo <56722059+ofepo@users.noreply.github.com>
2023-03-15 14:53:34 +0800

    Initial commit

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,45 @@
+import { navbar } from "vuepress-theme-hope";
+
+// 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
+export default navbar([
+  { text: "博客", icon: "blog", link: "/blog" },
+  {
+    text: "代码",
+    icon: "code",
+    prefix: "/",
+    children: [
+      "code/Markdown",
+      "code/AutoHotkey",
+      "code/Electron",
+      {
+        text: "页面开发",
+        icon: "vue",
+        prefix: "",
+        children: ["web/VuePress", "web/docsify", "deploy/VPS"],
+      },
+    ],
+  },
+  {
+    text: "应用",
+    icon: "app",
+    prefix: "/",
+    children: [
+      "apps/Applist",
+      { text: "ChatGPT", icon: "creative", link: "https://newzone.top/chatgpt/" },
+      { text: "直播手册", icon: "quote", link: "apps/livestreaming/1_obs_basic" },
+      {
+        text: "服务/系统",
+        icon: "any",
+        prefix: "",
+        children: ["services/NAS", "windows/faq"],
+      },
+    ],
+  },
+  {
+    text: "生活",
+    icon: "emmet",
+    prefix: "/family/",
+    children: ["Diet", "Shoppinglist", "Coupon"],
+  },
+  { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
+]);
